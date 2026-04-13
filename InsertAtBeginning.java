815a4d60d523fb5db0d04c9466e1f249efa7f07c
ZaraKhan7 <126021250+ZaraKhan7@users.noreply.github.com>
2026-04-13 11:14:37 +0500

    Implement linked list insertion at beginning

---
+++ InsertAtBeginning.java
@@ -0,0 +1,30 @@
+class Node {
+    String data;
+    Node next;
+
+    Node(String newdata) {
+        this.data = newdata;
+        this.next = null;
+    }
+}
+
+public class InsertAtBeginning{
+    public static void main(String[] args) {
+        Node head = new Node("Lahore");
+        head.next = new Node("Quetta");
+        head.next.next = new Node("Islamabad");
+        head.next.next.next = new Node("Balochistan");
+
+      // Insert at beginning
+        Node newNode =new Node("Karachi");
+        newNode.next = head;
+        head = newNode;
+        
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        
+        }
+}
+}   
