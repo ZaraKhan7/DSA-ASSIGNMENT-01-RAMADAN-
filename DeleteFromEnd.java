682392d5119b238980c03c6e8ed63996f6a6231c
ZaraKhan7 <126021250+ZaraKhan7@users.noreply.github.com>
2026-04-13 12:15:59 +0500

    Implement deletion at end of linked list

---
+++ DeleteFromEnd.java
@@ -0,0 +1,35 @@
+class Node {
+    String data;
+    Node next;
+
+    Node(String data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class DeleteFromEnd {
+    public static void main(String[] args) {
+
+        Node head = new Node("Karachi");
+        head.next = new Node("Lahore");
+        head.next.next = new Node("Quetta");
+        head.next.next.next = new Node("Islamabad");
+        head.next.next.next.next = new Node("Balochistan");
+
+        Node temp = head;
+
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = null;
+
+        // print
+        temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
